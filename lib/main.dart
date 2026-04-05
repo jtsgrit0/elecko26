d230c41b2a049faf901ff3a34bb4e95069100687
JTSGRIT0 <jtsgrit0@JTSGRITcBookPro.Davolink>
2026-04-06 08:15:54 +0900

    Add Firebase initialization and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter_application_1/firebase_options.dart';
 import 'package:flutter_application_1/app/app.dart';
 import 'package:flutter_application_1/app/injection_container.dart' as di;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
+  // Firebase 초기화
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
+  // Firebase 플러그인 인스턴스 로드
+  FirebaseAuth.instance;
+  FirebaseFirestore.instance;
+
   // 의존성 주입 설정
   await di.init();
 
