59c5d8cc6d176cf3521505d76ff397f66c39e855
JTSGRIT0 <jtsgrit0@JTSGRITcBookPro.Davolink>
2026-04-07 07:39:25 +0900

    Fix web app entrypoint import

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter_application_1/app/injection_container.dart' as di;
-import 'package:flutter_application_1/app/app.dart';
+import 'app/app.dart' as app;
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
   // 의존성 주입 설정
@@ -21,5 +21,5 @@ void main() async {
     print('DI init failed: $e\n$st');
   }
 
-  runApp(const MyApp());
+  runApp(const app.MyApp());
 }
