ff1f5eec536923259fa3e0daf5a05339642a0e14
JTSGRIT0 <jtsgrit0@JTSGRITcBookPro.Davolink>
2026-04-06 20:26:18 +0900

    Disable Firebase initialization on web to fix runtime error

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -9,14 +10,16 @@ import 'package:flutter_application_1/app/injection_container.dart' as di;
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   
-  // Firebase 초기화
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (!kIsWeb) {
+    // Firebase 초기화 (모바일/데스크톱만)
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
-  // Firebase 플러그인 인스턴스 로드
-  FirebaseAuth.instance;
-  FirebaseFirestore.instance;
+    // Firebase 플러그인 인스턴스 로드
+    FirebaseAuth.instance;
+    FirebaseFirestore.instance;
+  }
 
   // 의존성 주입 설정
   await di.init();
