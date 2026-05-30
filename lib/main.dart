e4a39db4245b0b4ef86d680164d69979f1f9e200
Taewoo Kim <taewoo.kim@git-flow.com>
2026-05-30 12:26:40 +0900

    Re-trigger build to fix compilation error

--- lib/main.dart
+++ lib/main.dart
@@ -18,5 +18,6 @@ void main() async {
   final getMembersUseCase = di.sl<GetMembersUseCase>();
   final members = await getMembersUseCase.call();
 
+// Re-trigger build
   runApp(MyApp(members: members));
 }
